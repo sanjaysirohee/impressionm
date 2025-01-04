22c0ab76d50792c874dbb580463a59361d29f0e2
sanjaysirohee <sanjay.sirohee@gmail.com>
2025-01-04 13:53:24 +0530

    d
    d

--- admin/database/connectdb.php
+++ admin/database/connectdb.php
@@ -1,15 +1,18 @@
 <?php
-/*
+$baseurl = $_SERVER['SERVER_NAME'];
+//echo $baseurl;
+if($baseurl=='localhost')
+{
 define("HOSTNAME","localhost");
 define("USERNAME","root");
 define("PASSWORD","");
 define("DATABASE","adotrippremiumdb");
-*/
+}else{
 define("HOSTNAME","localhost");
-define("USERNAME","veloxnc1_veloxndbun");
-define("PASSWORD","india@vbd121");
-define("DATABASE","veloxnc1_veloxndb");
-
+define("USERNAME","impressionmachinery_db");
+define("PASSWORD","9QazuqfcFSmUuNcZfdjb");
+define("DATABASE","impressionmachinery_db");
+}
 
 $con=new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE) or die("Unable to connect");
 ?>
