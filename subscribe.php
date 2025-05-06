3d73d08033eaa1878520b683c5f30c3969b31daa
todiwalritik <ritik.t@veloxn.com>
2025-05-06 18:29:10 +0530

    done
    done

--- subscribe.php
+++ subscribe.php
@@ -7,11 +7,7 @@ $sql = "INSERT INTO subscribed_customer (email) VALUES ('$email')";
 if ($conn->query($sql) === TRUE) {
   echo "Your message has been sent. Thank you, " . $email . ", we will contact you shortly.";
 
-<<<<<<< HEAD
   // Prepare the message for API
-=======
->>>>>>> 24e25e1f62a5225cc10ce49933371e0323034b77
-  
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
